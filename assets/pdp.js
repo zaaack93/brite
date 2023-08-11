c529520724440c95c049b6c0250cf95ed8ad4341
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-08-11 20:20:38 +0100

    PDP - fix scrolling functionality

--- assets/pdp.js
+++ assets/pdp.js
@@ -22,10 +22,3 @@ function updateURLAndRemoveParams() {
   
   // Call the function to update the URL and remove query parameters
   updateURLAndRemoveParams();
-
-  var sidebar = new StickySidebar('#sidebar', {
-    containerSelector: '.rdc-product-page-details',
-    innerWrapperSelector: '.inner-wrapper-sticky',
-    topSpacing: 10,
-    minWidth: 992
-});
